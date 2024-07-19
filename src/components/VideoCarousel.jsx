47d04b783fb1d367c1f4a692f5d8ed9051416b26
BlockbusterAndy <124876077+BlockbusterAndy@users.noreply.github.com>
2024-07-19 13:40:39 +0530

    ns
    na

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import {highlightsSlides} from '../constants'
+import { useRef, useState } from 'react'
 
 const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setvideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVidoe: false,
+    isPlaying: false,
+  })
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='flex items-center'>
+        { highlightsSlides.map((list, i) => (
+          <div key={list.id} id='slider' className='sm:pr-20 pr-10' >
+            <div className="video-carousel_container">
+              <div className="w-full h-full rounded-xl flex-center overflow-hidden bg-black">
+                <video id='video' playsInline={true} preload='auto' muted >
+                  <source src={list.video} type="video/mp4"/>
+                </video>
+              </div>
+              <div className='absolute top-12 left-[5%] z-10'>
+                {list.textLists.map((text, i) => (
+                  <p key={text} className='md:text-2xl text-xl font-medium' >{text}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+
+      </div>
+    </>
   )
 }
 
